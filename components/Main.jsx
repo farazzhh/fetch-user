d63bdf338b18f4d1f5ab51a418af3582d270aff3
farazzhh <havaee.faraz@gmail.com>
2021-10-01 21:03:03 +0330

    tailwind

---
+++ components/Main.jsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+
+export const getStaticProps = async() => {
+
+} 
+
+export const Main = () => {
+    return (
+        <div>
+            <h1 className="text-red-700">Home</h1>
+        </div>
+    )
+}
