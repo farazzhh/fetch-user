7b907c8391220c82814192ca6844f9d3217502e8
farazzhh <havaee.faraz@gmail.com>
2021-10-02 00:19:15 +0330

    loading

---
+++ components/Loading.jsx
@@ -0,0 +1,11 @@
+
+
+export const Loading = () => {
+    return (
+        <div  className="w-screen h-screen absolute top-0 left-0 z-50">
+            <div>
+                <h1>Loading . . .</h1>
+            </div>
+        </div>
+    )
+}
